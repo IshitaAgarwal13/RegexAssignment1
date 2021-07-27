b8714237983c695edc78b2e8e06e6817010d5e53
Ishita Agarwal <67784472+IshitaAgarwal13@users.noreply.github.com>
2021-07-27 16:02:03 +0530

    Update and rename Program5.java to Percentage.java

--- Percentage.java
+++ Percentage.java
@@ -5,7 +5,7 @@ division, >=40.00 AND <50.00 Third division, <40.00 Fail]  */
 package javaPack;
 import java.util.Scanner;
 
-public class DataTypeDemo {
+public class Percentage {
 
         public static void main(String[] args) {
                 // TODO Auto-generated method stub
