f97a54888d5df0c0e0d6c05a1c72d82c0ed610f9
Ishita Agarwal <67784472+IshitaAgarwal13@users.noreply.github.com>
2021-07-27 15:57:03 +0530

    Update and rename Program3.java to AddDimenson.java

--- AddDimenson.java
+++ AddDimenson.java
@@ -3,7 +3,7 @@
 package javaPack;
 import java.util.Scanner;
 
-public class DataTypeDemo {
+public class AddDimension {
 
         public static void main(String[] args) {
                 // TODO Auto-generated method stub
